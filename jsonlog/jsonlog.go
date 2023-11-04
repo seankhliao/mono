4c09c7bc6c64600e3d24684a6091ffcaa9190a87
Sean Liao <sean+git@liao.dev>
2023-11-04 19:37:54 +0000

    jsonlog and deps

---
+++ jsonlog/jsonlog.go
@@ -0,0 +1,452 @@
+package jsonlog
+
+import (
+	"context"
+	"encoding"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"slices"
+	"strconv"
+	"sync"
+	"time"
+	"unicode/utf8"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+const (
+	// magic numbers to reduce number of slice resizes
+	// slog holds 5 attrs
+	stateBufferSize = 1024
+
+	// used by appendString
+	hex = "0123456789abcdef"
+)
+
+var (
+
+	// shared
+	globalSep = []byte(",")
+
+	// assert it is a handler
+	_ slog.Handler = new(handler)
+
+	// reduce allocations in steady state
+	pool = &sync.Pool{
+		New: func() any {
+			s := make([]byte, 0, stateBufferSize)
+			return &s
+		},
+	}
+
+	// used by appendString
+	safeSet = [utf8.RuneSelf]bool{
+		' ':      true,
+		'!':      true,
+		'"':      false,
+		'#':      true,
+		'$':      true,
+		'%':      true,
+		'&':      true,
+		'\'':     true,
+		'(':      true,
+		')':      true,
+		'*':      true,
+		'+':      true,
+		',':      true,
+		'-':      true,
+		'.':      true,
+		'/':      true,
+		'0':      true,
+		'1':      true,
+		'2':      true,
+		'3':      true,
+		'4':      true,
+		'5':      true,
+		'6':      true,
+		'7':      true,
+		'8':      true,
+		'9':      true,
+		':':      true,
+		';':      true,
+		'<':      true,
+		'=':      true,
+		'>':      true,
+		'?':      true,
+		'@':      true,
+		'A':      true,
+		'B':      true,
+		'C':      true,
+		'D':      true,
+		'E':      true,
+		'F':      true,
+		'G':      true,
+		'H':      true,
+		'I':      true,
+		'J':      true,
+		'K':      true,
+		'L':      true,
+		'M':      true,
+		'N':      true,
+		'O':      true,
+		'P':      true,
+		'Q':      true,
+		'R':      true,
+		'S':      true,
+		'T':      true,
+		'U':      true,
+		'V':      true,
+		'W':      true,
+		'X':      true,
+		'Y':      true,
+		'Z':      true,
+		'[':      true,
+		'\\':     false,
+		']':      true,
+		'^':      true,
+		'_':      true,
+		'`':      true,
+		'a':      true,
+		'b':      true,
+		'c':      true,
+		'd':      true,
+		'e':      true,
+		'f':      true,
+		'g':      true,
+		'h':      true,
+		'i':      true,
+		'j':      true,
+		'k':      true,
+		'l':      true,
+		'm':      true,
+		'n':      true,
+		'o':      true,
+		'p':      true,
+		'q':      true,
+		'r':      true,
+		's':      true,
+		't':      true,
+		'u':      true,
+		'v':      true,
+		'w':      true,
+		'x':      true,
+		'y':      true,
+		'z':      true,
+		'{':      true,
+		'|':      true,
+		'}':      true,
+		'~':      true,
+		'\u007f': true,
+	}
+)
+
+func New(level slog.Level, out io.Writer) slog.Handler {
+	return &handler{
+		minLevel: level,
+		state:    new(state),
+		mu:       new(sync.Mutex),
+		w:        out,
+	}
+}
+
+type handler struct {
+	minLevel slog.Level
+	state    *state
+	mu       *sync.Mutex
+	w        io.Writer
+}
+
+func (h *handler) clone() *handler {
+	b0 := pool.Get().(*[]byte)
+	return &handler{
+		minLevel: h.minLevel,
+		state:    h.state.clone(*b0),
+		mu:       h.mu,
+		w:        h.w,
+	}
+}
+
+func (h *handler) Enabled(ctx context.Context, l slog.Level) bool {
+	return l >= h.minLevel
+}
+
+func (h *handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	if len(attrs) == 0 {
+		return h
+	}
+	h2 := h.clone()
+	for _, a := range attrs {
+		h2.state.attr(a)
+	}
+	return h2
+}
+
+func (h *handler) WithGroup(name string) slog.Handler {
+	if name == "" {
+		return h
+	}
+	h2 := h.clone()
+	h2.state.openGroup(name)
+	return h2
+}
+
+func (h *handler) Handle(ctx context.Context, r slog.Record) error {
+	// add attrs to state
+	b0 := pool.Get().(*[]byte)
+	defer func() { pool.Put(b0) }()
+	state := h.state.clone(*b0)
+	r.Attrs(func(a slog.Attr) bool {
+		state.attr(a)
+		return true
+	})
+	state.closeAll()
+
+	// initialize write buffer
+	var buf []byte
+	if cap(state.buf)-len(state.buf) < 160+len(r.Message) {
+		buf = make([]byte, 0, len(state.buf)+160+len(r.Message))
+	} else {
+		b1 := pool.Get().(*[]byte)
+		defer func() { pool.Put(b1) }()
+		buf = (*b1)[:0]
+	}
+
+	buf = append(buf, `{`...)
+
+	// time
+	if !r.Time.IsZero() {
+		buf = append(buf, `"time":"`...)
+		buf = r.Time.AppendFormat(buf, time.RFC3339Nano)
+		buf = append(buf, `",`...)
+	}
+	// level
+	buf = append(buf, `"level":"`...)
+	buf = append(buf, r.Level.String()...)
+	buf = append(buf, `"`...)
+
+	// trace
+	spanCtx := trace.SpanContextFromContext(ctx)
+	if spanCtx.IsValid() {
+		buf = append(buf, `,"trace_id":"`...)
+		buf = append(buf, spanCtx.TraceID().String()...)
+		buf = append(buf, `","span_id":"`...)
+		buf = append(buf, spanCtx.SpanID().String()...)
+		buf = append(buf, `"`...)
+
+	}
+	// any other special keys
+	// e.g. file:line, attrs from ctx or extracted during attr processing by state.attr
+
+	// message
+	buf = append(buf, `,"message":`...)
+	buf = appendString(buf, r.Message)
+
+	// attrs
+	if len(state.buf) > 0 {
+		buf = append(buf, `,`...)
+		buf = append(buf, state.buf...)
+	}
+	buf = append(buf, "}\n"...)
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	_, err := h.w.Write(buf)
+	return err
+}
+
+// state holds preformatted attributes
+type state struct {
+	confirmedLast int    // length of buf when we last wrote a complete attr
+	groupOpenIdx  []int  // indexes before open groups, allows rollback on empty groups
+	separator     []byte // separator to write before an attr or group
+	buf           []byte // buffer of preformatted contents
+	// TODO hold special keys to be placed in top level (eg error)
+}
+
+func (h *state) clone(buf []byte) *state {
+	if cap(h.buf) > stateBufferSize {
+		buf = slices.Clone(h.buf)
+	} else {
+		buf = buf[:len(h.buf)]
+		copy(buf, h.buf)
+	}
+	s := &state{
+		h.confirmedLast,
+		slices.Clone(h.groupOpenIdx),
+		slices.Clone(h.separator),
+		buf,
+	}
+	return s
+}
+
+func (h *state) openGroup(n string) {
+	h.groupOpenIdx = append(h.groupOpenIdx, len(h.buf)) // record rollback point
+	h.buf = append(h.buf, h.separator...)               // maybe need a separator
+	h.buf = appendString(h.buf, n)                      // key name
+	h.buf = append(h.buf, []byte(":{")...)              // open group
+	h.separator = nil                                   // no separator for first attr
+}
+
+func (h *state) closeGroup() {
+	lastGroupIdx := h.groupOpenIdx[len(h.groupOpenIdx)-1] // pop off the rollback point for current group
+	h.groupOpenIdx = h.groupOpenIdx[:len(h.groupOpenIdx)-1]
+	if h.confirmedLast > lastGroupIdx { // group was non empty
+		h.buf = append(h.buf, []byte("}")...) // close off the group
+		h.confirmedLast = len(h.buf)          // record new last point
+		return
+	}
+	h.buf = h.buf[:lastGroupIdx] // all open subgroups were empty, rollback
+}
+
+func (h *state) closeAll() {
+	for range h.groupOpenIdx {
+		h.closeGroup()
+	}
+	h.groupOpenIdx = nil
+}
+
+func (h *state) attr(attr slog.Attr) {
+	val := attr.Value.Resolve()  // handle logvaluer
+	if attr.Equal(slog.Attr{}) { // drop empty attr
+		return
+	} else if val.Kind() == slog.KindGroup { // recurse into group
+		g := val.Group()
+		if len(g) == 0 {
+			return
+		} else if attr.Key != "" { // inline empty keys
+			h.openGroup(attr.Key)
+		}
+		for _, a := range val.Group() {
+			h.attr(a)
+		}
+		if attr.Key != "" {
+			h.closeGroup()
+		}
+		return
+	} else if attr.Key == "" {
+		return
+	}
+	// TODO: grab any special keys
+
+	h.buf = append(h.buf, h.separator...)
+	h.separator = globalSep
+	h.buf = appendString(h.buf, attr.Key)
+	h.buf = append(h.buf, []byte(":")...)
+	switch val.Kind() {
+	case slog.KindAny:
+		switch v := val.Any().(type) {
+		case json.Marshaler:
+			b, _ := v.MarshalJSON()
+			h.buf = appendString(h.buf, b)
+		case encoding.TextMarshaler:
+			b, _ := v.MarshalText()
+			h.buf = appendString(h.buf, b)
+		case fmt.Stringer:
+			s := v.String()
+			h.buf = appendString(h.buf, s)
+		case error:
+			s := v.Error()
+			h.buf = appendString(h.buf, s)
+		default:
+			b, _ := json.Marshal(val.Any())
+			h.buf = append(h.buf, b...)
+		}
+	case slog.KindBool:
+		h.buf = strconv.AppendBool(h.buf, val.Bool())
+	case slog.KindDuration:
+		h.buf = append(h.buf, `"`...)
+		h.buf = append(h.buf, val.Duration().String()...)
+		h.buf = append(h.buf, `"`...)
+	case slog.KindFloat64:
+		h.buf = strconv.AppendFloat(h.buf, val.Float64(), 'f', -1, 64)
+	case slog.KindInt64:
+		h.buf = strconv.AppendInt(h.buf, val.Int64(), 10)
+	case slog.KindString:
+		h.buf = appendString(h.buf, val.String())
+	case slog.KindTime:
+		h.buf = append(h.buf, `"`...)
+		h.buf = val.Time().AppendFormat(h.buf, time.RFC3339Nano)
+		h.buf = append(h.buf, `"`...)
+	case slog.KindUint64:
+		h.buf = strconv.AppendUint(h.buf, val.Uint64(), 10)
+	default:
+		panic("unhandled kind" + val.Kind().String())
+	}
+	h.confirmedLast = len(h.buf)
+}
+
+// json string encoder copied from encoding/json
+
+func appendString[Bytes []byte | string](dst []byte, src Bytes) []byte {
+	dst = append(dst, '"')
+	start := 0
+	for i := 0; i < len(src); {
+		if b := src[i]; b < utf8.RuneSelf {
+			if safeSet[b] {
+				i++
+				continue
+			}
+			dst = append(dst, src[start:i]...)
+			switch b {
+			case '\\', '"':
+				dst = append(dst, '\\', b)
+			case '\b':
+				dst = append(dst, '\\', 'b')
+			case '\f':
+				dst = append(dst, '\\', 'f')
+			case '\n':
+				dst = append(dst, '\\', 'n')
+			case '\r':
+				dst = append(dst, '\\', 'r')
+			case '\t':
+				dst = append(dst, '\\', 't')
+			default:
+				// This encodes bytes < 0x20 except for \b, \f, \n, \r and \t.
+				// If escapeHTML is set, it also escapes <, >, and &
+				// because they can lead to security holes when
+				// user-controlled strings are rendered into JSON
+				// and served to some browsers.
+				dst = append(dst, '\\', 'u', '0', '0', hex[b>>4], hex[b&0xF])
+			}
+			i++
+			start = i
+			continue
+		}
+		// TODO(https://go.dev/issue/56948): Use generic utf8 functionality.
+		// For now, cast only a small portion of byte slices to a string
+		// so that it can be stack allocated. This slows down []byte slightly
+		// due to the extra copy, but keeps string performance roughly the same.
+		n := len(src) - i
+		if n > utf8.UTFMax {
+			n = utf8.UTFMax
+		}
+		c, size := utf8.DecodeRuneInString(string(src[i : i+n]))
+		if c == utf8.RuneError && size == 1 {
+			dst = append(dst, src[start:i]...)
+			dst = append(dst, `\ufffd`...)
+			i += size
+			start = i
+			continue
+		}
+		// U+2028 is LINE SEPARATOR.
+		// U+2029 is PARAGRAPH SEPARATOR.
+		// They are both technically valid characters in JSON strings,
+		// but don't work in JSONP, which has to be evaluated as JavaScript,
+		// and can lead to security holes there. It is valid JSON to
+		// escape them, so we do so unconditionally.
+		// See https://en.wikipedia.org/wiki/JSON#Safety.
+		if c == '\u2028' || c == '\u2029' {
+			dst = append(dst, src[start:i]...)
+			dst = append(dst, '\\', 'u', '2', '0', '2', hex[c&0xF])
+			i += size
+			start = i
+			continue
+		}
+		i += size
+	}
+	dst = append(dst, src[start:]...)
+	dst = append(dst, '"')
+	return dst
+}
---
+++ jsonlog/jsonlog_test.go
@@ -0,0 +1,328 @@
+package jsonlog
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"log/slog"
+	"math"
+	"math/big"
+	"net/netip"
+	"os"
+	"reflect"
+	"testing"
+	"testing/slogtest"
+	"time"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+func TestHandlerSlogtest(t *testing.T) {
+	t.Parallel()
+
+	buf := new(bytes.Buffer)
+	handler := New(slog.LevelInfo, buf)
+	err := slogtest.TestHandler(handler, func() []map[string]any {
+		all := buf.String()
+		dec := json.NewDecoder(buf)
+		var results []map[string]any
+		for dec.More() {
+			var result map[string]any
+			err := dec.Decode(&result)
+			if err != nil {
+				t.Errorf("unmarshal log: %v\n%v", err, all)
+				break
+			}
+			results = append(results, result)
+		}
+		return results
+	})
+	if err != nil {
+		t.Errorf("testhandler: %v", err)
+	}
+}
+
+func TestHandler(t *testing.T) {
+	t.Parallel()
+
+	tid, _ := trace.TraceIDFromHex("5b8aa5a2d2c872e8321cf37308d69df2")
+	sid, _ := trace.SpanIDFromHex("051581bf3cb55c13")
+	tcs := []struct {
+		name  string
+		args  []any
+		level slog.Level
+		msg   string
+		ctx   context.Context
+		want  map[string]any
+	}{
+		{
+			name: "simple message",
+			msg:  "a simple message!",
+			want: map[string]any{
+				"message": "a simple message!",
+				"level":   "INFO",
+			},
+		}, {
+			name: "complex message",
+			msg:  "msg with quotes \" and newlines \n and slash \\ and nul \x00",
+			want: map[string]any{
+				"message": "msg with quotes \" and newlines \n and slash \\ and nul \x00",
+				"level":   "INFO",
+			},
+		}, {
+			name: "ints",
+			msg:  "ints",
+			args: []any{[]slog.Attr{slog.Int("a", 0), slog.Int("b", 1), slog.Int("c", -1), slog.Int("d", math.MaxInt64)}},
+			want: map[string]any{
+				"message": "ints",
+				"level":   "INFO",
+				"a":       0.0,
+				"b":       1.0,
+				"c":       -1.0,
+				"d":       float64(math.MaxInt64),
+			},
+		}, {
+			name: "floats",
+			msg:  "floats",
+			args: []any{[]slog.Attr{slog.Float64("a", 0.0), slog.Float64("b", -1.0), slog.Float64("c", 1.1), slog.Float64("d", math.MaxFloat64)}},
+			want: map[string]any{
+				"message": "floats",
+				"level":   "INFO",
+				"a":       0.0,
+				"b":       -1.0,
+				"c":       1.1,
+				"d":       math.MaxFloat64,
+			},
+		}, {
+			name: "bools",
+			msg:  "bools",
+			args: []any{[]slog.Attr{slog.Bool("a", true), slog.Bool("b", false)}},
+			want: map[string]any{
+				"message": "bools",
+				"level":   "INFO",
+				"a":       true,
+				"b":       false,
+			},
+		}, {
+			name: "strings",
+			msg:  "strings",
+			args: []any{[]slog.Attr{slog.String("a", "hello world")}},
+			want: map[string]any{
+				"message": "strings",
+				"level":   "INFO",
+				"a":       "hello world",
+			},
+		}, {
+			name: "time",
+			msg:  "time",
+			args: []any{[]slog.Attr{slog.Time("a", time.Date(2023, 10, 16, 6, 4, 2, 123456789, time.UTC)), slog.Duration("b", 13*time.Hour+17*time.Minute+5*time.Second+734*time.Millisecond)}},
+			want: map[string]any{
+				"message": "time",
+				"level":   "INFO",
+				"a":       "2023-10-16T06:04:02.123456789Z",
+				"b":       "13h17m5.734s",
+			},
+		}, {
+			name: "simple groups",
+			msg:  "simple groups",
+			args: []any{[]any{slog.Group("a", slog.Int("b", 1))}, "c", "d", []slog.Attr{slog.Group("e", slog.Int("f", 1))}},
+			want: map[string]any{
+				"message": "simple groups",
+				"level":   "INFO",
+				"a": map[string]any{
+					"b": 1.0,
+				},
+				"c": map[string]any{
+					"d": map[string]any{
+						"e": map[string]any{
+							"f": 1.0,
+						},
+					},
+				},
+			},
+		}, {
+			name: "empty groups",
+			msg:  "empty groups",
+			args: []any{[]any{slog.Group("a")}, []any{slog.Group("b", slog.Group("c"))}, "d", []any{slog.Group("e")}, []slog.Attr{slog.Group("e", slog.Group("f"))}},
+			want: map[string]any{
+				"message": "empty groups",
+				"level":   "INFO",
+			},
+		}, {
+			name: "alternating",
+			msg:  "alternating",
+			args: []any{"a", []any{slog.Group("b", slog.Int("c", 1), slog.Group("d", slog.Int("e", 2), slog.Group("f"))), slog.Int("g", 3)}, "h", []any{slog.Int("i", 4), slog.Group("j", slog.Int("k", 5))}, "l", []slog.Attr{slog.Int("m", 6), slog.Group("n", slog.Group("o")), slog.Int("p", 7)}},
+			want: map[string]any{
+				"message": "alternating",
+				"level":   "INFO",
+				"a": map[string]any{
+					"b": map[string]any{
+						"c": 1.0,
+						"d": map[string]any{
+							"e": 2.0,
+						},
+					},
+					"g": 3.0,
+					"h": map[string]any{
+						"i": 4.0,
+						"j": map[string]any{
+							"k": 5.0,
+						},
+						"l": map[string]any{
+							"m": 6.0,
+							"p": 7.0,
+						},
+					},
+				},
+			},
+		}, {
+			name: "context trace",
+			msg:  "context trace",
+			ctx:  trace.ContextWithSpanContext(context.Background(), trace.NewSpanContext(trace.SpanContextConfig{TraceID: tid, SpanID: sid})),
+			want: map[string]any{
+				"message":  "context trace",
+				"level":    "INFO",
+				"trace_id": "5b8aa5a2d2c872e8321cf37308d69df2",
+				"span_id":  "051581bf3cb55c13",
+			},
+		},
+	}
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			buf := new(bytes.Buffer)
+			lg := slog.New(New(slog.LevelDebug, buf))
+			for i, arg := range tc.args {
+				if i == len(tc.args)-1 {
+					break
+				}
+				switch v := arg.(type) {
+				case string:
+					lg = lg.WithGroup(v)
+				case []any:
+					lg = lg.With(v...)
+				}
+			}
+			if len(tc.args) > 0 {
+				lg.LogAttrs(tc.ctx, tc.level, tc.msg, tc.args[len(tc.args)-1].([]slog.Attr)...)
+			} else {
+				lg.LogAttrs(tc.ctx, tc.level, tc.msg)
+			}
+
+			//
+			var got map[string]any
+			err := json.Unmarshal(buf.Bytes(), &got)
+			if err != nil {
+				t.Errorf("unmarshaling log line: %v", err)
+			}
+			delete(got, "time")
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("\ngot = %v\nwnt = %v", got, tc.want)
+				for k := range got {
+					if got[k] != tc.want[k] {
+						t.Errorf("diff: %v %+v %+v", k, got[k], tc.want[k])
+					}
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkHandler(b *testing.B) {
+	ctx := context.Background()
+	handlers := map[string]*slog.Logger{
+		"slog":    slog.New(slog.NewJSONHandler(io.Discard, &slog.HandlerOptions{})),
+		"jsonlog": slog.New(New(slog.LevelDebug, io.Discard)),
+	}
+	for name, lg := range handlers {
+		b.Run(name+"/ints", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				lg.LogAttrs(ctx, slog.LevelInfo, "benchmark msg", slog.Int("a", 1), slog.Int("b", 2), slog.Int("c", 3), slog.Int("d", 4), slog.Int("e", 5))
+			}
+		})
+		b.Run(name+"/floats", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				lg.LogAttrs(ctx, slog.LevelInfo, "benchmark msg", slog.Float64("a", 1), slog.Float64("b", 2), slog.Float64("c", 3), slog.Float64("d", 4), slog.Float64("e", 5))
+			}
+		})
+		b.Run(name+"/bools", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				lg.LogAttrs(ctx, slog.LevelInfo, "benchmark msg", slog.Bool("a", true), slog.Bool("b", true), slog.Bool("c", false), slog.Bool("d", true), slog.Bool("e", true))
+			}
+		})
+		b.Run(name+"/strings", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				lg.LogAttrs(ctx, slog.LevelInfo, "benchmark msg", slog.String("a", "zzzzzz"), slog.String("b", "yyy yyy"), slog.String("c", "x x x x x x"), slog.String("d", "w w ww w w"), slog.String("e", "vvv v vvv"))
+			}
+		})
+		b.Run(name+"/objects", func(b *testing.B) {
+			err := errors.New("zzz")      // error
+			ip := netip.IPv6Unspecified() // encoding.TextMarshaler
+			bi := big.NewInt(1234567890)  // json.Marshaler
+			zz := struct {
+				A string
+				B int
+			}{
+				"aaa", 4567,
+			}
+			for i := 0; i < b.N; i++ {
+				lg.LogAttrs(ctx, slog.LevelInfo, "benchmark msg", slog.Any("a", err), slog.Any("b", ip), slog.Any("c", bi), slog.Any("d", fs.ModeDir), slog.Any("e", zz))
+			}
+		})
+		b.Run(name+"/mixed", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				lg.LogAttrs(ctx, slog.LevelInfo, "benchmark msg", slog.Int("a", 1), slog.Float64("b", 2), slog.Bool("c", true), slog.String("d", "benchy"), slog.Duration("e", 12345678*time.Millisecond))
+			}
+		})
+	}
+}
+
+func FuzzHandler(f *testing.F) {
+	f.Fuzz(func(t *testing.T, lines uint8, level, level2 int, nargs uint64, i1, i2, i3, i4, i5, i6, i7, i8, i9, i0, msg string) {
+		strs := []string{i0, i1, i2, i3, i4, i5, i6, i7, i8, i9}
+		buf := new(bytes.Buffer)
+		lg := slog.New(New(slog.Level(level), buf))
+		fmt.Fprintln(os.Stderr, lines, level, level2, nargs, msg)
+		for i := uint8(0); i < lines; i++ {
+			nlg := lg
+			nargs := nargs * uint64(lines)
+			var args []any
+			for nargs > 0 {
+				switch nargs % 6 {
+				case 0:
+					nlg = nlg.With(args...)
+					args = nil
+				case 1:
+					nlg = nlg.WithGroup(strs[nargs%10])
+				case 2:
+					args = append(args, strs[nargs%10], strs[(nargs*7)%10])
+				case 4:
+					args = append(args, strs[nargs%10], nargs)
+				case 5:
+					args = append(args, strs[nargs%10], nargs%2 == 0)
+				case 6:
+					lop := int(nargs) % (len(args) / 2)
+					args = append(args[:lop*2], strs[nargs%10], slog.Group(strs[(nargs*13)%10], args[lop*2:]...))
+				}
+				nargs /= 6
+			}
+			nlg.Log(context.Background(), slog.Level(level2), msg, args...)
+		}
+
+		all := buf.String()
+		fmt.Fprintln(os.Stderr, all)
+		dec := json.NewDecoder(buf)
+		for dec.More() {
+			var out any
+			err := dec.Decode(&out)
+			if err != nil {
+				t.Error(err, all)
+			}
+		}
+	})
+}
