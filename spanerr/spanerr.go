5aa5df4d68af528e718ab6d8c56ac261930ac8df
Sean Liao <sean@liao.dev>
2024-07-02 18:47:31 +0100

    spanerr: add docs

--- spanerr/spanerr.go
+++ spanerr/spanerr.go
@@ -8,6 +8,8 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+// Err wraps the error in the given message,
+// and also records the information as part of the [trace.Span].
 func Err(span trace.Span, msg string, err error, attrs ...attribute.KeyValue) error {
 	span.RecordError(err, trace.WithAttributes(attrs...))
 	span.SetStatus(codes.Error, msg)
