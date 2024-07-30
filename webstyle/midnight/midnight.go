742284005ca6dd8f3bc35f79bf1a25893c272e7d
Sean Liao <sean+git@liao.dev>
2024-07-30 01:32:38 +0100

    webstyle/midnight: fix color
    Change-Id: I78713009d41c4c5f57bd10e02eff3d34f29769a6

--- webstyle/midnight/midnight.go
+++ webstyle/midnight/midnight.go
@@ -3,22 +3,40 @@ package midnight
 import "github.com/alecthomas/chroma/v2"
 
 func Style() (*chroma.Style, error) {
-	b := chroma.NewStyleBuilder("midnight")
-	b.Add(chroma.Background, "#080c10")
-	// b.Add(chroma.LineHighlight, "")
-	// b.Add(chroma.Error, "")
-	b.Add(chroma.Keyword, "#a665d0")
-	// b.Add(chroma.KeywordType, )
-	b.Add(chroma.Name, "#9ac6e0")
-	b.Add(chroma.NameAttribute, "#7ac098")
-	b.Add(chroma.NameFunction, "#c8b670")
-	b.Add(chroma.Literal, "#5080ff")
-	b.Add(chroma.LiteralString, "#e0a076")
-	b.Add(chroma.LiteralNumber, "#5080ff")
-	b.Add(chroma.Operator, "#ff7279")
-	b.Add(chroma.Comment, "#878d96")
-	// b.Add(chroma.Generic, "")
-	b.Add(chroma.Text, "#b5bdc5")
-	// b.Add(chroma.Punctuation, )
-	return b.Build()
+	return chroma.NewStyleBuilder("midnight").
+		AddEntry(chroma.Background, chroma.StyleEntry{
+			Colour:     chroma.ParseColour("#eceff1"),
+			Background: chroma.ParseColour("#303030"),
+		}).
+		AddEntry(chroma.Keyword, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#a665d0"),
+		}).
+		AddEntry(chroma.Name, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#9ac6e0"),
+		}).
+		AddEntry(chroma.NameAttribute, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#7ac098"),
+		}).
+		AddEntry(chroma.NameFunction, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#c8b670"),
+		}).
+		AddEntry(chroma.Literal, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#5080ff"),
+		}).
+		AddEntry(chroma.LiteralString, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#e0a076"),
+		}).
+		AddEntry(chroma.LiteralNumber, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#5080ff"),
+		}).
+		AddEntry(chroma.Operator, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#ff7279"),
+		}).
+		AddEntry(chroma.Comment, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#878d96"),
+		}).
+		AddEntry(chroma.Text, chroma.StyleEntry{
+			Colour: chroma.ParseColour("#b5bdc5"),
+		}).
+		Build()
 }
