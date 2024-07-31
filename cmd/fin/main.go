e2c463eac298389dddd5a2151299e844b85e82d5
Sean Liao <sean+git@liao.dev>
2024-08-01 00:40:10 +0100

    cmd/fin: push and pull subcommands
    Change-Id: I1e11144bd02f7b77259d0b03469e6cbc5c7b04e3

--- cmd/fin/main.go
+++ cmd/fin/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"crypto/md5"
 	"flag"
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
+	"strings"
 	"text/tabwriter"
 
 	"go.seankhliao.com/mono/cmd/fin/findata"
 	"go.seankhliao.com/mono/ycli"
+	"gocloud.dev/blob"
+	_ "gocloud.dev/blob/gcsblob"
 )
 
 func main() {
@@ -16,6 +24,8 @@ func main() {
 		"fin is a custom tool to track expenses",
 		func(fs *flag.FlagSet) {},
 		ViewCommand(),
+		PushCommand(),
+		PullCommand(),
 	))
 }
 
@@ -118,4 +128,128 @@ func printTable(w io.Writer, sum []findata.Summary, name string, names []string)
 		month.MarshalTSV(w, name, names)
 	}
 	fmt.Fprintln(w)
+}
+
+func PushCommand() ycli.Command {
+	var bucketName string
+	var localGlob string
+	return ycli.New(
+		"push",
+		"upload local data to a storage bucket",
+		func(fs *flag.FlagSet) {
+			fs.StringVar(&bucketName, "bucket", "gs://fin-liao-dev", "bucket identifier")
+			fs.StringVar(&localGlob, "glob", "*.fin.cue", "a glob pattern patching local files")
+		},
+		func(stdout, stderr io.Writer) error {
+			err := runPush(stdout, bucketName, localGlob)
+			if err != nil {
+				return fmt.Errorf("push: %w", err)
+			}
+			return nil
+		},
+	)
+}
+
+func runPush(stdout io.Writer, bucketName, localGlob string) error {
+	files, err := filepath.Glob(localGlob)
+	if err != nil {
+		return fmt.Errorf("match local files %q: %w", localGlob, err)
+	}
+
+	ctx := context.Background()
+	bkt, err := blob.OpenBucket(ctx, bucketName)
+	if err != nil {
+		return fmt.Errorf("open bucket %q: %q", bucketName, err)
+	}
+
+	for _, file := range files {
+		b, err := os.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		localMD5 := md5.Sum(b)
+
+		fileName := filepath.Base(file)
+		attrs, err := bkt.Attributes(ctx, fileName)
+		if err == nil {
+			if bytes.Equal(localMD5[:], attrs.MD5) {
+				fmt.Fprintln(stdout, "skipping unchanged file", file)
+				continue
+			}
+		}
+
+		err = bkt.WriteAll(ctx, fileName, b, &blob.WriterOptions{
+			ContentType: "text/plain",
+		})
+		if err != nil {
+			return fmt.Errorf("upload %q: %w", file, err)
+		}
+
+		fmt.Fprintln(stdout, "uploaded", file)
+	}
+	return nil
+}
+
+func PullCommand() ycli.Command {
+	var bucketName string
+	return ycli.New(
+		"pull",
+		"download remote data from a storage bucket",
+		func(fs *flag.FlagSet) {
+			fs.StringVar(&bucketName, "bucket", "gs://fin-liao-dev", "bucket identifier")
+		},
+		func(stdout, stderr io.Writer) error {
+			err := runPull(stdout, bucketName)
+			if err != nil {
+				return fmt.Errorf("push: %w", err)
+			}
+			return nil
+		},
+	)
+}
+
+func runPull(stdout io.Writer, bucketName string) error {
+	ctx := context.Background()
+	bkt, err := blob.OpenBucket(ctx, bucketName)
+	if err != nil {
+		return fmt.Errorf("open bucket %q: %q", bucketName, err)
+	}
+
+	items := bkt.List(&blob.ListOptions{})
+	for {
+		item, err := items.Next(ctx)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return fmt.Errorf("list bucket %q: %w", bucketName, err)
+		} else if !strings.HasSuffix(item.Key, ".fin.cue") {
+			continue
+		}
+
+		fileName := filepath.Base(item.Key)
+
+		b, err := os.ReadFile(fileName)
+		if err == nil {
+			localMD5 := md5.Sum(b)
+			if bytes.Equal(localMD5[:], item.MD5) {
+				fmt.Fprintln(stdout, "skipping unchanged file", item.Key)
+				continue
+			}
+		}
+
+		b, err = bkt.ReadAll(ctx, item.Key)
+		if err != nil {
+			return fmt.Errorf("read %q: %w", item.Key, err)
+		}
+
+		err = os.WriteFile(fileName, b, 0o644)
+		if err != nil {
+			return fmt.Errorf("write %q: %w", fileName, err)
+		}
+
+		fmt.Fprintln(stdout, "downloaded", item.Key)
+	}
+
+	return nil
 }
