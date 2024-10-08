525857ac32b7bcf244ffb5bd1bc38733c934bf7c
Sean Liao <sean+git@liao.dev>
2024-10-09 00:16:55 +0100

    upload command
    Change-Id: Ib80caeb80543a24d25b41a4cc719dfc8270942fd

---
+++ _data/config/generate.go
@@ -0,0 +1,5 @@
+//go:build upload
+
+package main
+
+//go:generate gcloud storage cp moo.cue gs://config-liao-dev/moo.cue
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:build !(lint || deps || codegen)
+//go:build !(lint || deps || codegen || upload)
 
 package main
 
@@ -8,7 +8,8 @@ import "fmt"
 func main() {
 	fmt.Println("run one of:")
 	fmt.Println()
-	fmt.Println("\t", "go generate -x -tags lint")
-	fmt.Println("\t", "go generate -x -tags deps")
 	fmt.Println("\t", "go generate -x -tags codegen")
+	fmt.Println("\t", "go generate -x -tags deps")
+	fmt.Println("\t", "go generate -x -tags lint")
+	fmt.Println("\t", "go generate -x -tags upload")
 }
