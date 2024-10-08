15dc4c28af767107361cad824ce918ebfb0cf985
Sean Liao <sean+git@liao.dev>
2024-10-08 21:31:20 +0100

    merge ghdefaults
    Change-Id: Ie82f988810a72e9891905a2d33b6ca976eab52ee

--- cmd/moo/app.go
+++ cmd/moo/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"go.seankhliao.com/mono/cmd/moo/ghdefaults"
 	"go.seankhliao.com/mono/cmd/moo/homepage"
 	"go.seankhliao.com/mono/cmd/moo/reqlog"
 	"go.seankhliao.com/mono/webstyle/webstatic"
@@ -12,28 +13,31 @@ import (
 )
 
 type Config struct {
-	ReqLog   reqlog.Config
-	Homepage homepage.Config
+	GHDefaults ghdefaults.Config
+	Homepage   homepage.Config
+	ReqLog     reqlog.Config
 }
 
 type App struct {
-	ReqLog   *reqlog.App
-	Homepage *homepage.App
+	GHDefaults *ghdefaults.App
+	Homepage   *homepage.App
+	ReqLog     *reqlog.App
 }
 
 func New(ctx context.Context, c Config, o yrun.O11y) (a *App, err error) {
 	a = &App{}
-	a.ReqLog, err = reqlog.New(c.ReqLog, o)
+	a.GHDefaults, err = ghdefaults.New(c.GHDefaults, o)
 	a.Homepage, err = homepage.New(c.Homepage, o)
-
+	a.ReqLog, err = reqlog.New(c.ReqLog, o)
 	return a, nil
 }
 
 func Register(a *App, r yrun.HTTPRegistrar) {
 	webstatic.Register(r)
 
-	reqlog.Register(a.ReqLog, r)
+	ghdefaults.Register(a.GHDefaults, r)
 	homepage.Register(a.Homepage, r)
+	reqlog.Register(a.ReqLog, r)
 
 	r.Pattern("GET", "", "/{$}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello world")
