925fc5ef858bb2c698d130fe4674b0a925431ea5
Sean Liao <sean+git@liao.dev>
2024-10-08 21:59:45 +0100

    merge earbug
    Change-Id: I26472779bd69946a137a081404bd56ffdd649327

--- cmd/moo/app.go
+++ cmd/moo/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"go.seankhliao.com/mono/cmd/moo/earbug"
 	"go.seankhliao.com/mono/cmd/moo/ghdefaults"
 	"go.seankhliao.com/mono/cmd/moo/homepage"
 	"go.seankhliao.com/mono/cmd/moo/reqlog"
@@ -13,12 +14,14 @@ import (
 )
 
 type Config struct {
+	Earbug     earbug.Config
 	GHDefaults ghdefaults.Config
 	Homepage   homepage.Config
 	ReqLog     reqlog.Config
 }
 
 type App struct {
+	Earbug     *earbug.App
 	GHDefaults *ghdefaults.App
 	Homepage   *homepage.App
 	ReqLog     *reqlog.App
@@ -26,15 +29,29 @@ type App struct {
 
 func New(ctx context.Context, c Config, o yrun.O11y) (a *App, err error) {
 	a = &App{}
+	a.Earbug, err = earbug.New(c.Earbug, o)
+	if err != nil {
+		return nil, err
+	}
 	a.GHDefaults, err = ghdefaults.New(c.GHDefaults, o)
+	if err != nil {
+		return nil, err
+	}
 	a.Homepage, err = homepage.New(c.Homepage, o)
+	if err != nil {
+		return nil, err
+	}
 	a.ReqLog, err = reqlog.New(c.ReqLog, o)
+	if err != nil {
+		return nil, err
+	}
 	return a, nil
 }
 
 func Register(a *App, r yrun.HTTPRegistrar) {
 	webstatic.Register(r)
 
+	earbug.Register(a.Earbug, r)
 	ghdefaults.Register(a.GHDefaults, r)
 	homepage.Register(a.Homepage, r)
 	reqlog.Register(a.ReqLog, r)
