85933f980f98f185770a840f1af760110acb4ae3
Sean Liao <sean@liao.dev>
2025-08-27 23:03:22 +0100

    cmd/cli/gosdkupdate: wip refactor

--- cmd/cli/gosdkupdate/main.go
+++ cmd/cli/gosdkupdate/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"debug/buildinfo"
 	"flag"
@@ -14,8 +15,8 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"slices"
 	"strings"
+	"sync"
 
 	"go.seankhliao.com/mono/goreleases"
 	"go.seankhliao.com/mono/ycli"
@@ -36,6 +37,8 @@ type App struct {
 	parallel  int
 	bootstrap string
 
+	updateTip   bool
+	updateGo    bool
 	updateTools bool
 }
 
@@ -43,52 +46,14 @@ func (a *App) register(fset *flag.FlagSet) {
 	fset.IntVar(&a.keepMinor, "keep-minor", 3, "number of released minor versions to keep")
 	fset.IntVar(&a.parallel, "parallel", 4, "parallel downloads")
 	fset.StringVar(&a.bootstrap, "bootstrap", "/usr/bin/go", "path to bootstrap go")
-	fset.BoolVar(&a.updateTools, "update-tools", true, "update tools in GOBIN")
+	fset.BoolVar(&a.updateTip, "tip", true, "install tip (dev) go version")
+	fset.BoolVar(&a.updateGo, "go", true, "update go sdks")
+	fset.BoolVar(&a.updateTools, "tools", true, "update tools in GOBIN")
 }
 
 func (a *App) run(stdout, stderr io.Writer) error {
 	ctx := context.Background()
 
-	// get current releases
-	rels, err := goreleases.Releases(http.DefaultClient, ctx, "", true)
-	if err != nil {
-		return fmt.Errorf("get go releases: %w", err)
-	}
-
-	// map release versions to language versions
-	keepVer := make([]string, 0, a.keepMinor)
-	lastLang := ""
-	for _, rel := range rels {
-		if lang := version.Lang(rel.Version); lang != lastLang {
-			lastLang = lang
-			keepVer = append(keepVer, rel.Version)
-		}
-		if len(keepVer) >= a.keepMinor {
-			break
-		}
-	}
-
-	// clean up ~/sdk
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return fmt.Errorf("get user home: %w", err)
-	}
-	sdkPath := filepath.Join(home, "sdk")
-	des, err := os.ReadDir(sdkPath)
-	if err != nil {
-		return fmt.Errorf("get installed sdks: %w", err)
-	}
-	for _, de := range des {
-		if !strings.HasPrefix(de.Name(), "go1") {
-			// sub repos
-			continue
-		}
-		if !slices.Contains(keepVer, de.Name()) {
-			os.RemoveAll(filepath.Join(sdkPath, de.Name()))
-		}
-	}
-
-	// clean up gobin
 	gobin := os.Getenv("GOBIN")
 	if gobin == "" {
 		gobin = filepath.Join(os.Getenv("GOPATH"), "bin")
@@ -96,58 +61,68 @@ func (a *App) run(stdout, stderr io.Writer) error {
 	if gobin == "" {
 		return fmt.Errorf("can't deduce GOBIN")
 	}
-	os.RemoveAll(filepath.Join(gobin, "go"))
-	os.RemoveAll(filepath.Join(gobin, "gotip"))
-
-	des, err = os.ReadDir(gobin)
-	if err != nil {
-		return fmt.Errorf("get installed sdk shims: %w", err)
-	}
-	for _, de := range des {
-		if !strings.HasPrefix(de.Name(), "go1") {
-			// sub repos
-			continue
+	if a.updateTip || a.updateGo {
+		des, err := os.ReadDir(gobin)
+		if err != nil {
+			return fmt.Errorf("dead dir %s: %w", gobin, err)
 		}
-		if !slices.Contains(keepVer, de.Name()) {
-			os.RemoveAll(filepath.Join(gobin, de.Name()))
-		}
-	}
-
-	os.Setenv("GOENV", "off")
-	os.Setenv("GOFLAGS", "-trimpath")
-
-	// download versions
-	sem := make(chan struct{}, a.parallel)
-	for _, sdk := range append(keepVer, "gotip") {
-		sem <- struct{}{}
-		go func() {
-			defer func() { <-sem }()
-			errInstall := a.installSDK(ctx, gobin, sdk)
-			if errInstall != nil {
-				fmt.Fprintln(stderr, "error installing", sdk, errInstall)
-				return
+		for _, de := range des {
+			if a.downloadTip && de.Name() == "go" {
+				os.RemoveAll(filepath.Join(gobin, "go"))
 			}
-			fmt.Fprintln(stdout, "downloaded", sdk)
-		}()
+			if a.downloadGo && strings.HasPrefix(de.Name(), "go1") {
+				os.RemoveAll(filepath.Join(gobin, de.Name()))
+			}
+		}
 	}
 
-	// wait
-	for range a.parallel {
-		sem <- struct{}{}
+	var wg sync.WaitGroup
+	if a.updateTip {
+		wg.Go(func() {
+			err := a.downloadTip(ctx)
+			if err != nil {
+					fmt.Fprintln(stderr, "download go", ver, err)
+			}
+
+		})
 	}
-	// clear for reuse
-	for range a.parallel {
-		<-sem
+	if a.updateGo {
+		rels, err := goreleases.Releases(http.DefaultClient, ctx, "", true)
+		if err != nil {
+			return fmt.Errorf("get go releases: %w", err)
+		}
+
+		// map release versions to language versions
+		keepVer := make([]string, 0, a.keepMinor)
+		lastLang := ""
+		for _, rel := range rels {
+			if lang := version.Lang(rel.Version); lang != lastLang {
+				lastLang = lang
+				keepVer = append(keepVer, rel.Version)
+			}
+			if len(keepVer) >= a.keepMinor {
+				break
+			}
+		}
+
+
+		for _, ver := range keepVer {
+			wg.Go(func() {
+				err := a.downloadGo(ctx, ver)
+				if err != nil {
+					fmt.Fprintln(stderr, "download go", ver, err)
+				}
+			})
+		}
 	}
+
+	wg.Wait()
 
 	if !a.updateTools {
 		return nil
 	}
 
-	os.Unsetenv("GOENV")
-	os.Unsetenv("GOFLAGS")
-
-	des, err = os.ReadDir(gobin)
+	des, err := os.ReadDir(gobin)
 	if err != nil {
 		return fmt.Errorf("list installed go tools: %w", err)
 	}
@@ -197,8 +172,30 @@ func (a *App) installSDK(ctx context.Context, gobin, sdk string) error {
 		return fmt.Errorf("download sdk: %w", err)
 	}
 
-	if sdk == "gotip" {
-		os.Symlink(filepath.Join(gobin, "gotip"), filepath.Join(gobin, "go"))
+	err = os.Symlink(filepath.Join(gobin, "gotip"), filepath.Join(gobin, "go"))
+	if err != nil {
+		return fmt.Errorf("set gotip as default go: %w", err)
+	}
+	return nil
+}
+
+func (a *App) installVersioned(ctx context.Context, gobin, ver string) error {
+	cmd := exec.CommandContext(ctx, a.bootstrap, "version")
+	cmd.Env = append(cmd.Env, fmt.Sprintf("GOTOOLCHAIN=%s", ver))
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("download toolchain: %w", err)
+	}
+	cmd = exec.CommandContext(ctx, a.bootstrap, "env", "GOROOT")
+	cmd.Env = append(cmd.Env, fmt.Sprintf("GOTOOLCHAIN=%s", ver))
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("get downloaded goroot: %w", err)
+	}
+	b = bytes.TrimSpace(b)
+	err = os.Symlink(filepath.Join(string(b), "bin/go"), filepath.Join(gobin, "go"+ver))
+	if err != nil {
+		return fmt.Errorf("link to downloaded: %w", err)
 	}
 	return nil
 }
