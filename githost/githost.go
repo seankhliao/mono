f1eb97545ec52976f76622069cd29babd490470a
Sean Liao <sean@liao.dev>
2026-04-11 23:53:13 +0100

    githost: logo

--- githost/githost.go
+++ githost/githost.go
@@ -35,7 +35,7 @@ virtual-root=/
 js=/static/cgit/cgit.js
 css=/static/cgit/cgit.css
 favicon=/static/cgit/favicon.ico
-logo=/static/cgitcgit.png
+logo=https://seankhliao.com/static/icon-192.png
 `
 )
 
